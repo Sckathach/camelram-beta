1b70a15c9701995a1bbb94e98eecf9153daac6d8
Sckathach <sckathach@hackademint.org>
2023-04-08 09:19:44 +0200

    Added vars function to count variables used in AST

--- calc.ml
+++ calc.ml
@@ -15,5 +15,8 @@ let rec eval = function
     | EInt(i) -> i
     | EBinOp(op, e1, e2) -> (ex_op op) (eval e1) (eval e2);;
 
-
+let rec vars = function
+    | EInt(i) -> []
+    | EVar(x) -> [x]
+    | EBinOp(op, e1, e2) -> (vars e1) @ (vars e2);;
 
