600ff38be66ad8fcde1b63cb17487ea32d6107f3
Sckathach <sckathach@hackademint.org>
2023-04-23 11:24:51 +0200

    Added variables

--- calc.ml
+++ calc.ml
@@ -1,11 +1,12 @@
 open Ast
+open Variable
 
 let addition x y = x + y;;
 let subtraction x y = x - y;;
 let multiplication x y = x * y;;
 
 (* Points to the implementation of the function *)
-let ex_op = function
+let fun_of_bop = function
     | BopAdd -> addition
     | BopSub -> subtraction
     | BopMul -> multiplication;;
@@ -13,10 +14,9 @@ let ex_op = function
 (* Evaluate the tree, not everything is already implemented so the pattern matching is not exhaustive*)
 let rec eval = function
     | EInt(i) -> i
-    | EBinOp(op, e1, e2) -> (ex_op op) (eval e1) (eval e2);;
+    | EBinOp(op, e1, e2) -> (fun_of_bop op) (eval e1) (eval e2);;
 
 let rec vars = function
     | EInt(i) -> []
     | EVar(x) -> [x]
     | EBinOp(op, e1, e2) -> (vars e1) @ (vars e2);;
-
---
+++ variable.ml
@@ -0,0 +1,26 @@
+module Variable = struct
+    open Map
+    type value =
+        | VInt of int
+        | VFloat of float
+
+    module Dict = Map.Make(String)
+
+    let variables = ref Dict.empty;;
+    variables := Dict.add "pi" (VFloat(3.14)) !variables
+
+    let to_string = function
+        | None -> "La variable n'existe pas."
+        | Some(s) -> match s with
+            | VInt(x) -> string_of_int x
+            | VFloat(x) -> string_of_float x
+
+    let get key =
+        try Some(Dict.find key !variables) with
+            Not_found -> None
+
+    let add_float name value =
+        variables := Dict.add name (VFloat(value)) !variables
+    let add_int name value =
+        variables := Dict.add name (VInt(value)) !variables
+end
