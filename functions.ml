7ae8cb28de437d134675856a814adfe829150315
Winninger Thomas <sckathach@hackademint.org>
2023-04-26 10:05:25 +0200

    Update functions.ml

--- functions.ml
+++ functions.ml
@@ -25,22 +25,3 @@ let power a b =
   | VFloat(x), VInt(y) -> VFloat(x ** (float_of_int y))
   | VFloat(x), VFloat(y) -> VFloat(x ** y)
 ;;
-
-
-
-
-
-
-
-
-(** Fonction pow_int
-    @returns a^b
-    @type val pow_int : int -> int -> int = <fun>
-*)
-let rec pow_int (a : int) (b : int) =
-    match b mod 2, b with
-        | _, 0 -> 1
-        | _, 1 -> a
-        | 0, _ -> pow_int a (b / 2) * pow_int a (b / 2)
-        | 1, _ -> a * pow_int a (b - 1)
-;;
